1d5058db67ab91991e16a3a39a70ffea41b261ea
david-laugh <matda8080@gmail.com>
2023-07-31 08:53:26 +0900

    Update Option

--- app.py
+++ app.py
@@ -1,18 +1,61 @@
 from io import BytesIO
+import os, signal
 
 from paper_translator.usecase.get_paper_copus import execute as gpc
 
 from pywebio import start_server
-from pywebio.input import file_upload
-from pywebio.output import put_file
+from pywebio.input import input_group, file_upload, select
+from pywebio.output import popup, put_html, put_file, put_buttons, put_text
+from pywebio.session import hold, set_env, eval_js
+
+
+def restart():
+    eval_js("window.open('http://localhost:2030/', '_blank')")
+    eval_js("window.close()")
+
+
+def end():
+    eval_js("window.close()")
+    os.kill(os.getpid(), signal.SIGTERM)
 
 
 def App():
-    pdf = file_upload("Select a Paper(.pdf):", accept=".pdf")
-    filename = pdf["filename"][:-4] + "_translated.txt"
-    content = pdf["content"]
-    translated = gpc(BytesIO(content))
-    put_file(filename, translated.encode(), 'download me')
+    set_env(title="Paper Translator")
+
+    try:
+        options = input_group("Options", [
+            file_upload("Select a Paper(.pdf):", accept=".pdf", name='paper'),
+            select('language', ['chinese', 'korean'], name='language'),
+            select('mode', ['elaborate', 'fast'], name='mode'),
+        ])
+
+        filename = options["paper"]["filename"][:-4] + "_translated.txt"
+        content = options["paper"]["content"]
+        translated = gpc(BytesIO(content), options["language"], options["mode"])
+
+        put_file(filename, translated.encode(), 'download me')
+
+        put_text("", scope='ROOT')
+        put_buttons(
+            [dict(
+                label="재시작",
+                value='restart',
+                color='success'
+            ), 
+            dict(
+                label="종료",
+                value='end',
+                color='success'
+            )],
+            onclick=[restart, end],
+            outline=True, scope='ROOT')
+
+        hold()
+
+    except Exception as e:
+        popup(f'{e}', [
+            put_html('<h3>프로그램을 종료합니다.</h3>'),
+        ])
 
 
 if __name__ == "__main__":
--- src/paper_translator/usecase/get_paper_copus.py
+++ src/paper_translator/usecase/get_paper_copus.py
@@ -1,23 +1,46 @@
+import time
+
+import googletrans
 from easynmt import EasyNMT
 
+from pywebio.output import put_progressbar, set_progressbar
 from paper_translator.domain import Corpus, Sentence
 
 
-def execute(pdf_path):
-    model = EasyNMT('m2m_100_418M')
+def execute(pdf_path, language, mode):
+    if mode == "fast":
+        model = googletrans.Translator()
+    else:
+        model = EasyNMT('m2m_100_418M')
+
+    if language == "chinese":
+        target_lang = 'zh-cn'
+        if mode == "elaborate":
+            target_lang = 'zh'
+    else:
+        target_lang = 'ko'
 
     pdf_corpus = Corpus(pdf_path)
     sentences = Sentence(pdf_corpus.text)
     translated = ""
-    for sentence in sentences[:]:
+
+    put_progressbar('bar');
+    for i, sentence in enumerate(sentences[:]):
         try:
-            translated_sentence = model.translate(sentence, target_lang='ko', source_lang='en')
+            if mode == "elaborate":
+                translated_sentence = model.translate(
+                    sentence, target_lang=target_lang, source_lang='en')
+            else:
+                translated_sentence = model.translate(
+                    sentence, dest=target_lang, src='en')
+                time.sleep(1)
             translated += f"{sentence}\n{translated_sentence}\n"
         except Exception as e:
             print(f"{e}")
+        set_progressbar('bar', i / len(sentences[:]))
     return translated
 
 
 if __name__ == "__main__":
     pdf_path = "example/1812.00285.pdf"
-    execute(pdf_path)
+    execute(pdf_path, 'korean', 'fast')
